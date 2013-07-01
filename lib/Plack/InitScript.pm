98565a0548e99759f3736929833b3d7d9354bb03
Konstantin S. Uvarin <khedin@gmail.com>
2013-07-01 22:16:29 +0400

    Initial commit

---
+++ lib/Plack/InitScript.pm
@@ -0,0 +1,111 @@
+package Plack::InitScript;
+
+use 5.006;
+use strict;
+use warnings;
+
+=head1 NAME
+
+Plack::InitScript - The great new Plack::InitScript!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Plack::InitScript;
+
+    my $foo = Plack::InitScript->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 SUBROUTINES/METHODS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Konstantin S. Uvarin, C<< <khedin at gmail.com> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-plack-initscript at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Plack-InitScript>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Plack::InitScript
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker (report bugs here)
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Plack-InitScript>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Plack-InitScript>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Plack-InitScript>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Plack-InitScript/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2013 Konstantin S. Uvarin.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Plack::InitScript
